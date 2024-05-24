7ea6ccccb0be29650278955f68cf9cdb85c242bf
Fabbye3737 <fabian.besca01@e-uvt.ro>
2024-05-24 23:24:55 +0300

    Update

---
+++ Spatii_Verzi.sql
@@ -0,0 +1,72 @@
+create extension postgis;
+
+create table if not exists parc(
+	fid serial primary key,
+	nume varchar (100) not null,
+	scop varchar (100) null,
+	geom geometry ('Polygon', 4326) not null
+);
+
+create table if not exists evenimente(
+	fid serial primary key,
+	tip_eveniment varchar (100) not null,
+	data date null
+);
+
+create table evenimente_parc(
+	fid serial primary key,
+	parc_fid integer not null,
+	evenimente_fid integer not null,
+	foreign key(parc_fid) references parc(fid),
+	foreign key(evenimente_fid) references evenimente(fid)
+);
+
+create table if not exists sistem_irigare(
+	fid serial primary key,
+	parc_fid integer not null,
+	numar_conducte integer not null,
+	geom geometry ('LineString', 4326) not null,
+	foreign key(parc_fid) references parc(fid)
+);
+
+create table if not exists alei(
+	fid serial primary key,
+	parc_fid integer not null,
+	latime_m smallint not null,
+	lungime_m integer not null,
+	geom geometry ('LineString', 4326) not null,
+	foreign key(parc_fid) references parc(fid)
+);
+
+create table if not exists mobilier_urban(
+	fid serial primary key,
+	parc_fid integer not null,
+	tip_mobilier varchar not null,
+	scop varchar not null,
+	geom geometry ('Point', 4326) not null,
+	foreign key(parc_fid) references parc(fid)
+);
+
+create table if not exists constructii(
+	fid serial primary key,
+	parc_fid integer not null,
+	tip_consctructie varchar not null,
+	geom geometry ('Point', 4326) not null,
+	foreign key(parc_fid) references parc(fid)
+);
+
+create table if not exists vegetatie(
+	fid serial primary key,
+	parc_fid integer not null,
+	tip_vegetatie varchar not null,
+	permanenta bool not null,
+	geom geometry ('Point', 4326) not null,
+	foreign key(parc_fid) references parc(fid)
+);
+
+create table if not exists salubritate(
+	fid serial primary key,
+	parc_fid integer not null,
+	nume_companie varchar not null,
+	foreign key(parc_fid) references parc(fid)
+);
